b9ed239760293e14b0449635d4e34aee4bc64ef7
Teoh Sing Jian <1191103209@student.mmu.edu.my>
2023-06-23 05:49:57 +0800

    change font type

--- libs/VRButton.js
+++ libs/VRButton.js
@@ -188,7 +188,7 @@ class VRButton{
         element.style.borderRadius = '4px';
         element.style.background = (active) ? 'rgba(20,150,80,1)' : 'rgba(180,20,20,1)';
         element.style.color = '#fff';
-        element.style.font = `normal ${fontSize}px sans-serif`;
+        element.style.font = `normal ${fontSize}px ITC Avant Garde Gothic`;
         element.style.textAlign = 'center';
         element.style.opacity = '0.5';
         element.style.outline = 'none';
